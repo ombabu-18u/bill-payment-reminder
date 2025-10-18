c2c3a0ab70a3b7a1c53875807bfe217733c05767
Ombabu Chaurasiya <ombabusmacbook@Ombabus-MacBook-Air.local>
2025-10-19 00:55:42 +0530

    feat: Initialize Spring Boot project with basic configuration

---
+++ src/main/java/com/billreminder/system/BillPaymentReminderApplication.java
@@ -0,0 +1,13 @@
+package com.billreminder.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillPaymentReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillPaymentReminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billreminder/system/BillPaymentReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billreminder.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillPaymentReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
